684f79223e8dfae0ef2da427dec181063f3a4884
Prasanth Galla <gallaprashanth@gmail.com>
2024-03-17 04:14:49 -0400

    configure eslint, prettier.

---
+++ src/theme.tsx
@@ -0,0 +1,21 @@
+// src/theme.tsx
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#556cd6',
+    },
+    secondary: {
+      main: '#19857b',
+    },
+    error: {
+      main: '#ff0000',
+    },
+    background: {
+      default: '#f4f5fd',
+    },
+  },
+});
+
+export default theme;
